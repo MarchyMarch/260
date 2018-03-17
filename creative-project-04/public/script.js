8135ef94e26359a882471f9018f1439afcffa6a1
Kyle March <kcmarch23@gmail.com>
2018-03-16 19:54:16 -0600

    started on creative project and did a majority of the javascript

---
+++ creative-project-04/public/script.js
@@ -0,0 +1,245 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		items: [],
+		computers: [],
+		make: '',
+		model: '',
+		os: '',
+		screen: '',
+		processor: '',
+		ram: 0,
+		storageSize: 0,
+		storageType: '',
+		graphics: '',
+		description: '',
+		ramOptions: [
+			{id: "2", label: "2 GB", value: 2},
+			{id: "4", label: "4 GB", value: 4},
+			{id: "8", label: "8 GB", value: 8},
+			{id: "16", label: "16 GB", value: 16}
+		],
+		sizeOptions: [
+			{id: "128", label: "128 GB", value: 128},
+			{id: "256", label: "256 GB", value: 256},
+			{id: "500", label: "500 GB", value: 500},
+			{id: "750", label: "750 GB", value: 750},
+			{id: "1000", label: "1 TB", value: 1000}
+		],
+		person: '',
+		typeOptions: [
+			{id: "hdd", label: "HDD", value: "HDD"},
+			{id: "ssd", label: "SSD", value: "SSD"}
+		],
+		show: 'all',
+	},
+	created: function(){
+		this.getAll();
+	},
+	computed: {
+		availableItems: function(){
+			this.items.filter(function(item){
+				return !item.checkedout;
+			});
+			this.computers.filter(function(computer){
+				return !computer.checkedout;
+			});
+		},
+		// TODO: filter out computers seperately
+		filteredItems: function(){
+			if(this.show === 'checkedout'){
+				this.items.filter(function(item){
+					return item.checkedout;
+				})
+				this.computers.filter(function(computer){
+					return computer.checkedout;
+				})
+			}
+			if(this.show === 'available'){
+				this.items.filter(function(item){
+					return !item.checkedout;
+				});
+				this.computers.filter(function(computer){
+					return !computer.checkedout;
+				});
+			}
+
+			this.items;
+			this.computers;
+		}
+	},
+	methods: {
+		showAll: function() {this.show = 'all';},
+		showCheckedOut: function() {this.show = 'checkedout';},
+		showAvailable: function() {this.show = 'available';},
+		getAll: function(){
+			this.getItems();
+			this.getComputers();
+		},
+		getItems: function(){
+			axios.get("/api/items").then(response =>{
+				this.items = response.data;
+				return true;
+			}).catch(err =>{
+				console.log(err);
+			});
+		},
+		getComputers: function(){
+			axios.get("/api/computers").then(response =>{
+				this.computers = response.data;
+				return true;
+			}).catch(err =>{
+				console.log(err);
+			})
+		},
+		addItem: function() {
+			axios.post("/api/items", {
+				make: this.make,
+				model: this.model,
+				description: this.description,
+				checkedout: false,
+			}).then(response => {
+				this.make = '';
+				this.model = '';
+				this.description = '';
+				this.getItems();
+				return true;
+			}).catch(err => {
+				console.log(err);
+			});
+		},
+		addComputer: function(){
+			axios.post("/api/computers", {
+				make: this.make,
+				model: this.model,
+				os: this.os,
+				screen: this.screen,
+				processor: this.processor,
+				ram: this.ram,
+				storageSize: this.storageSize,
+				storageType: this.storageType,
+				graphics: this.graphics,
+				description: this.description,
+				checkedout: false,
+			}).then(response => {
+				this.make = '';
+				this.model = '';
+				this.os = '';
+				this.screen = '';
+				this.processor = '';
+				this.ram = 0;
+				this.storageSize = 0;
+				this.storageType = '';
+				this.graphics = '';
+				this.description = '';
+				this.getComputers();
+				return true;
+			}).catch(err => {
+				console.log(err);
+			});
+		},
+		checkOutComputer: function(computer){
+			var isPerson = this.getPerson();
+			if(isPerson){
+				axios.put("/api/computers/" + computer.id, {
+					make: computer.make,
+					model: computer.model,
+					os: computer.os,
+					screen: computer.screen,
+					processor: computer.processor,
+					ram: computer.ram,
+					storageSize: computer.storageSize,
+					storageType: computer.storageType,
+					graphics: computer.graphics,
+					description: computer.description,
+					checkedout: true,
+					person: this.person,
+				}).then(response => {
+					this.person = '';
+					return true;
+				}).catch(err => {
+					console.log(err);
+				});
+			}
+		},
+		checkOutItem: function(item){
+			var isPerson = this.getPerson();
+			if(isPerson){
+				axios.put("/api/items/" + item.id, {
+					make: item.make,
+					model: item.model,
+					description: item.description,
+					checkedout: true,
+					person: this.person,
+				}).then(response => {
+					this.person = '';
+					return true;
+				}).catch(err => {
+					console.log(err);
+				});
+			}
+		},
+		deleteComputer: function(computer){
+			if(confirm("Are you sure you want to delete this computer?")){
+				axios.delete("/api/computers/" + computer.id).then(response => {
+					this.getComputers();
+					return true;
+				}).catch(err => {
+					console.log(err);
+				});
+			}
+		},
+		deleteItem: function(item){
+			if(confirm("Are you sure you want to delete this item?")){
+				axios.delete("/api/items/" + item.id).then(response => {
+					this.getItems();
+					return true;
+				}).catch(err => {
+					console.log(err);
+				});	
+			}
+		},
+		getPerson: function(){
+			this.person = prompt("Who is checking out?");
+			if(this.person == null || this.person == ''){
+				return false;
+			}
+			else{
+				return true;
+			}
+		},
+		returnComputer: function(computer){
+			axios.put("/api/computers/" + computer.id, {
+				make: computer.make,
+				model: computer.model,
+				os: computer.os,
+				screen: computer.screen,
+				processor: computer.processor,
+				ram: computer.ram,
+				storageSize: computer.storageSize,
+				storageType: computer.storageType,
+				graphics: computer.graphics,
+				description: computer.description,
+				checkedout: false,
+				person: '',
+			}).then(response => {
+				return true;
+			}).catch(err => {
+				console.log(err);
+			});
+		},
+		returnItem: function(computer){
+			axios.put("/api/items/" + item.id, {
+				make: item.make,
+				model: item.model,
+				description: item.description,
+				checkedout: false,
+				person: '',
+			}).then(response => {
+				return true;
+			}).catch(err => {
+				console.log(err);
+			});
+		},
+	}
+})
---
+++ creative-project-04/server.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Set up an api app for items and computers
+const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('public'));
+
+// define item and computer array
+// set the computer and item ids and start at 0
+let items = [];
+let itemID = 0;
+
+let computers = [];
+let computerID = 0;
+
+// Set up the CRUD for items
+app.get('/api/items', (req, res) => {
+	console.log("Items requested at " + new Date().toLocaleString());
+	res.send(items);
+});
+
+app.post('/api/items', (req, res) =>{
+	console.log("Item added at " + new Date().toLocaleString());
+	itemID = itemID + 1;
+	let item = {id:id, make:req.body.make, model:req.body.model, description:req.body.description, checkedout:req.body.checkedout, person:req.body.person};
+	items.push(item);
+	res.send(item);
+});
+
+app.put('/api/items/:id', (req, res) => {
+	let id = parseInt(req.params.id);
+	let itemsMap = items.map(item => {return item.id});
+	let index = itemsMap.indexOf(id);
+	let item = items[index];
+	item.make = req.body.make;
+	item.model = req.body.model;
+	item.description = req.body.description;
+	item.checkedout = req.body.checkedout;
+	res.send(item);
+});
+
+app.delete('/api/items/:id', (req, res) => {
+	let id = parseInt(req.params.id);
+	let removeIndex = items.map(item => {return item.id}).indexOf(id);
+	if(removeIndex === -1){
+		res.status(404).send("The item does not exist");
+		return;
+	}
+	items.splice(removeIndex, 1);
+	res.sendStatus(200);
+});
+
+// Set up CRUD for computers
+app.get('/api/computers', (req, res) => {
+	console.log("Computers requested at " + new Date().toLocaleString());
+	res.send(computers);
+})
+
+app.post('/api/computers', (req, res) => {
+	console.log("Computer added at " + new Date().toLocaleString());
+	computerID = computerID + 1;
+	let computer = {id:id, make:req.body.make, model:req.body.model, os:req.body.os, screen:req.body.screen, processor:req.body.processor, ram:req.body.ram, storageSize:req.body.storageSize, storageType:req.body.storageType, graphics:req.body.graphics, description:req.body.description, checkedout:req.body.checkedout, person:req.body.person};
+	computers.push(computer);
+	res.send(computer);
+});
+
+app.put('/api/computers/:id', (req, res) => {
+	let id = parseInt(req.params.id);
+	let computersMap = computers.map(item => {return item.id});
+	let index = computersMap.indexOf(id);
+	let computer = computers[index];
+	computer.make = req.body.make;
+	computer.model = req.body.model;
+	computer.os = req.body.os;
+	computer.screen = req.body.screen;
+	computer.processor = req.body.processor;
+	computer.ram = req.body.ram;
+	computer.storageSize = req.body.storageSize;
+	computer.storageType = req.body.storageType;
+	computer.graphics = req.body.graphics;
+	computer.description = req.body.description;
+	computer.checkedout = req.body.checkedout;
+	res.send(computer);
+});
+
+app.delete('/api/computers/:id', (req, res) => {
+	let id = parseInt(req.params.id);
+	let removeIndex = computers.map(item => {return item.id}).indexOf(id);
+	if(removeIndex === -1){
+		res.status(404).send("The item does not exist");
+		return;
+	}
+	computers.splice(removeIndex, 1);
+	res.sendStatus(200);
+});
+
+app.listen(5000, () => console.log('Server started and listening on port 5000!'));
